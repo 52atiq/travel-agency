efcd708a1d6041f2f17dac64d82b2e29dee5ca4d
unknown <atiqit52@gmail.com>
2022-09-27 23:37:54 +0600

    filter done

--- src/pages/Destination/Destination.js
+++ src/pages/Destination/Destination.js
@@ -1,76 +1,112 @@
-import React, { useState } from 'react';
-// import React, { useState } from react;
+import React, { useState } from "react";
 
-import IsoTopeGrid from "react-isotope";
-
-import cardsLayout from "./cardsLayout.json";
-import filters from "./filters.json";
+const menu = [
+  {
+    id: 0,
+    image:
+      "https://media.istockphoto.com/photos/breakfast-with-bacon-eggs-pancakes-and-toast-picture-id533645537?k=20&m=533645537&s=612x612&w=0&h=KJXCpAo9XQvMI_djcnRMSsz_Y7OGS6z3-8VThxWyR0Q=",
+    name: "maggi",
+    category: "breakfast",
+    price: "$12",
+    description: "lorem vai",
+  },
+  {
+    id: 1,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZKXNZaiz_zfsm7lEZEd14mYrr91_3Bhe8AmXZdtpQ&s",
+    name: "maggi",
+    category: "evening",
+    price: "$12",
+    description: "lorem vai",
+  },
+  {
+    id: 2,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiFvpBt117-86L6r7QvhviFCMZnqjDuyux25I3QwOY&s",
+    name: "maggi",
+    category: "breakfast",
+    price: "$12",
+    description: "lorem vai 1",
+  },
+  {
+    id: 3,
+    image:
+      "https://image.shutterstock.com/image-photo/happy-multigeneration-family-gathering-around-260nw-1685806537.jpg",
+    name: "alli",
+    category: "evening",
+    price: "$42",
+    description: "lorem vai 2",
+  },
+  {
+    id: 4,
+    image:
+      "https://res.cloudinary.com/dtpgi0zck/image/upload/s--eWjIe4k---/c_fill,h_260,w_380/v1/EducationHub/photos/lightning-bolts.jpg",
+    name: "corm",
+    category: "dinner",
+    price: "$12",
+    description: "lorem vai 3",
+  },
+  {
+    id: 5,
+    image: "https://static01.nyt.com/images/2014/12/11/technology/personaltech/11machin-illo/11machin-illo-superJumbo-v3.jpg",
+    name: "pokga",
+    category: "lunch",
+    price: "$12",
+    description: "lorem vai 4",
+  },
+];
 
 const Destination = () => {
-     // Local state for managing filtering logic
-  const [filters, updateFilters] = useState(filtersDefault);
+  const [items, setItems]= useState(menu);
 
-  // Filter change handler
-  const onFilter = event => {
-    const {
-      target: { value, checked }
-    } = event;
+  const filterItem =(categItem)=> {
+    const updateItems = menu.filter((curElem) =>{
+      return curElem.category === categItem;
+    });
+    setItems(updateItems);
+  }
 
-    updateFilters(state =>
-      state.map(f => {
-        if (f.label === value) {
-          return {
-            ...f,
-            isChecked: checked
-          };
-        }
+  return (
+    <div className="container mx-auto mt-9">
+      <h2 className="text-center">Order your favourite dish</h2>
+      <div className="flex justify-center items-center gap-5 font-extrabold">
+        <button className="text-amber-500 p-2 bg-amber-400 rounded-lg text-white" onClick={() => filterItem('breakfast')}> Breakfast</button>
+        <button className="text-amber-500 p-2 bg-amber-400 rounded-lg text-white" onClick={() => filterItem('evening')}> Lunch</button>
+        <button className="text-amber-500 p-2 bg-amber-400 rounded-lg text-white" onClick={() => filterItem('dinner')}> Evening</button>
+        <button className="text-amber-500 p-2 bg-amber-400 rounded-lg text-white" onClick={() => filterItem('lunch')}> Dinner</button>
+        <button className="text-amber-500 p-2 bg-amber-400 rounded-lg text-white" onClick={() => setItems(menu)}> All</button>
+      </div>
 
-        return f;
-      })
-    );
-  };
+      {/* my main items section  */}
+      <div className="">
+          <div className=" grid grid-cols-1 md:grid-cols-3 rounded-md gap-8 mt-5">
+          {
+            items.map((elem) =>{
+                      const {id,name,image,description, price} =elem;
 
-        return (
-            <div className="App">
-              {/*Filter component*/}
-              <div className="filter-container">
-                {filters.map(f => (
-                  <div className="filter" key={`${f.label}_key`}>
-                    <input
-                      id={f.label}
-                      type="checkbox"
-                      value={f.label}
-                      onChange={onFilter}
-                      checked={f.isChecked}
-                    />
-                    <label htmlFor={f.label}>{f.label}</label>
-                  </div>
-                ))}
-              </div>
-        
-              <div className="container">
-                <IsoTopeGrid
-                  gridLayout={cardsLayout} // gridlayout of cards
-                  noOfCols={3} // number of columns show in one row
-                  unitWidth={200} // card width of 1 unit
-                  unitHeight={100} // card height of 1 unit
-                  filters={filters} // list of selected filters
-                >
-                  {cardsLayout.map(card => (
-                    <div key={card.id} className={card.filter[0]}>
-                      {card.id}
-                    </div>
-                  ))}
-                </IsoTopeGrid>
-              </div>
-            </div>
-          );
-        }
+                      return(
+                        <div className="flex flex-wrap gap-4 border border-green-600">
+                        <img className="flex justify-center items-center w-[150px] h-[90px]" src={image} />
+                        <h3> {name}</h3>
+                        <p>{description}</p>
+                      </div>
+                        
+                      )
+            }) 
+              
+            
+          }
+         
+          </div>
 
+      </div>
+
+
+
+
+
+    </div>
+  );
+};
 
 export default Destination;
-
-  
- 
-
-  
